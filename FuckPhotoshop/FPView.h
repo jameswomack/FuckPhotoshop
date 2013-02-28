7ab34a92ef991cb45394db6daaa2696f6b247700
James Womack <jamesw@amcoonline.net>
2013-02-27 20:39:09 -0800

    ignorance

---
+++ FuckPhotoshop/FPView.h
@@ -0,0 +1,13 @@
+//
+//  FPView.h
+//  FuckPhotoshop
+//
+//  Created by James Womack on 2/27/13.
+//  Copyright (c) 2013 James Womack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FPView : UIView
+
+@end
---
+++ FuckPhotoshop/FPView.m
@@ -0,0 +1,31 @@
+//
+//  FPView.m
+//  FuckPhotoshop
+//
+//  Created by James Womack on 2/27/13.
+//  Copyright (c) 2013 James Womack. All rights reserved.
+//
+
+#import "FPView.h"
+
+@implementation FPView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
